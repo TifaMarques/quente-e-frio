62e03f2df3e8a39556b65e24a471833d9f12f919
TifaMarques <tifany.siqueira@escola.pr.gov.br>
2025-05-30 10:26:44 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,44 @@
-let x;
-let y;
+let x; //criando variavel x
+let y; //criando variavel y
 
 function setup() {
   createCanvas(400, 400);
-  x = random(400);
-  x = int(x);
-  y = random(400);
+  x = random(400); //sorteando numero de 0 a 400
+  x = int(x); //transformando para numero inteiro
+  y = random(400); //mesma coisa que o de cima
   y = int(y);
 }
 
 function draw() {
-  background("lightblue");//Fundo da tela azul claro
-  x = x + random(-2,2);//Ajuste aleatório do ponto oculto
-  y = y + random(-2,2);//Ajuste aleatório do ponto oculto
-    fill("lightgreen")//Cor do círculo
-
-  let distanciaX;//Cateto
-  let distanciaY;//Cateto
-  let distancia;//Hipotenusa
-  distanciaX = mouseX - x; //Diferença de distância X
-  distanciaY = mouseY - y;//Diferença de distância Y
-  distancia = dist(mouseX,mouseY,x,y)//Função nativa para calcular distância
-
-  circle(mouseX, mouseY, distancia);
-  dist(mouseX,mouseY,x,y)
-
-  if (distancia < 3) {// Se posição X do mouse for igual a X
-    fill("purple")//Cor do texto
-    text("Encontrei!", 200, 200);//Então mostra o texto encontrei
-    noLoop();
-  }
-   if (distancia > 150){
-   fill ("blue")
-   circle (mouseX, mouseY, distancia)
-} else if (distancia > 100){
-    fill ("yellow")
-    circle (mouseX,mouseY,distancia)
-} else if (distancia > 75){
-  fill ("orange")
-  circle (mouseX,mouseY,distancia)
-} else if (distancia > 15){
-    fill("red")
-    circle (mouseX,mouseY,distancia)
-  }
-  }
+  background("rgb(234,184,234)"); //cor de fundo roxo
+ x=x+random(-2,2);
+ y=y+random(-2,2)
+  x=constrain(x,0,400)
+  y=constrain(y,0,400)
+   let distanciaX;
+    let distanciaY;
+    let distancia;
+    distanciaX = mouseX - x;
+    distanciaY = mouseY - y;
+    distancia = sqrt(distanciaX * distanciaX + distanciaY * distanciaY);
+  
+  
+   circle(mouseX,mouseY,distancia); //circulo com coordenadas aleatorias e diametro 10
+  
+  if(distancia>50)
+    fill("blue")
+  if(distancia<50)
+     fill("orange")
+   if(distancia<35)
+     fill("red")
+  
+                     
+    
+  
+  
+    if(distancia<3) { //se posição X do mouse for igual a X
+    text("ENCONTREI!",200,200); //entao mostrara o texto "encontrei
+    noLoop()
+    
+    }
+}
